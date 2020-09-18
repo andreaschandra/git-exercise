6f278b717b38a3f1cb0091707311d8e2e92aadd2
andreas chandra <phoenix.ichigo@yahoo.com>
2020-09-18 15:48:18 +0700

    add unit test

---
+++ test/main_test.py
@@ -0,0 +1,20 @@
+"""
+python test
+"""
+from unittest import TestCase
+
+class TryTesting(TestCase):
+    """
+    Class for TryTesting
+    """
+    def test_always_passes(self):
+        """
+        unit test always passes
+        """
+        self.assertTrue("name".upper() == "NAME")
+
+    def test_always_fails(self):
+        """
+        test always fails
+        """
+        self.assertTrue(3 == 4)
