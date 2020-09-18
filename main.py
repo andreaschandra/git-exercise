3e745f73c2213d9091ac0ae3bc1e316de7505df9
andreas chandra <phoenix.ichigo@yahoo.com>
2020-09-18 15:36:49 +0700

    add variable

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-import pandas as pd
+"""
+The main file that you have to run to get the output
+"""
+
+def main():
+    """
+	main function
+	"""
+    value_x = 1
+    return value_x
+
+if __name__ == "__main__":
+    main()
